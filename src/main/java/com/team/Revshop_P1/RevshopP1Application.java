dc93c518fa07f9662c833bacef336b2ff2916ba5
Faize <faize@gmail.com>
2024-10-18 22:08:48 +0530

    first commit

---
+++ src/main/java/com/team/Revshop_P1/RevshopP1Application.java
@@ -0,0 +1,14 @@
+package com.team.Revshop_P1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevshopP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevshopP1Application.class, args);
+		System.out.print("Hello Team");
+	}
+
+}
